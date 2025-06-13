504f4aab329ef6c48fda288787aef9da7b9efbc0
shubham kumar <ss7937601@gmail.com>
2025-06-13 20:17:11 +0530

    browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 
+// import React from "react";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import AnimatedRoutes from "./AnimatedRoutes";
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <AnimatedRoutes/>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimatedRoutes from "./AnimatedRoutes";
 
 function App() {
   return (
-    <Router>
-      <AnimatedRoutes/>
+    <Router basename="/Solar-Power-Estimator">
+      <AnimatedRoutes />
     </Router>
   );
 }
